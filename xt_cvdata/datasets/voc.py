880ea10c0009a5b3af243eabc104becceba61b88
Keegan Lensink <keegan@xtract.ai>
2019-08-30 16:21:30 -0700

    add pascal VOC and nyu depth dataset classes

---
+++ xt_cvdata/datasets/voc.py
@@ -0,0 +1,86 @@
+import torch.utils.data as data
+from PIL import Image, ImageDraw
+import os
+import os.path
+import sys
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+class VOC(data.Dataset):
+    def __init__(self, root, transform=None, target_transform=None, image_set='train'):
+        self.root = root
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self._image_set = image_set
+        self._imsetpath = os.path.join(self.root, 'ImageSets', 'Segmentation', '%s.txt')
+        self._annopath = os.path.join(self.root, 'SegmentationClass', '%s.png')
+        self._impath = os.path.join(self.root, 'JPEGImages', '%s.jpg')
+ 
+        with open(self._imsetpath % self._image_set) as f:
+            self.ids = f.readlines()
+        self.ids = [x.strip('\n') for x in self.ids]
+
+        self.labels = [
+            'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat',
+            'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant',
+            'sheep', 'sofa', 'train', 'tvmonitor', 'void'
+        ]
+        self.cmap = self._color_map()
+
+    def __getitem__(self, index):
+        img_id = self.ids[index]
+        target = Image.open(self._annopath % img_id)
+        img = Image.open(self._impath % img_id).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        return img, target
+
+    def __len__(self):
+        return len(self.ids)
+
+    def _color_map(self, normalized=True, base_map_name='tab20'):
+        base_map = matplotlib.cm.get_cmap(base_map_name, 22).colors
+        cmap = np.zeros_like(base_map)
+        cmap[0,-1] = 1
+        cmap[1:-1] = base_map[:20]
+        cmap[-1] = [1, 1, 1, 1]
+
+        return matplotlib.colors.ListedColormap(cmap)
+    
+
+if __name__ == '__main__':
+
+    from torchvision import transforms
+    from mytransforms import ToLabel, Relabel
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from utils import plottable
+
+    data_transforms = transforms.Compose([
+        transforms.CenterCrop(256),
+        transforms.ToTensor()
+    ])
+    label_transforms = transforms.Compose([
+        transforms.CenterCrop(256),
+        ToLabel(),
+        Relabel(255, 21)
+    ])
+
+    dataset = VOC('/home/klensink/GIT/SemanticSegmentation/data/VOC2012', transform=data_transforms, target_transform=label_transforms)
+
+    loader = data.DataLoader(dataset)
+
+    for i,(image, label) in enumerate(loader):
+        plt.imshow(plottable(image))
+        plt.figure()
+        plt.imshow(plottable(label), cmap=plt.cm.gist_ncar)
+        plt.show()
+        raise Exception('test')
+        
